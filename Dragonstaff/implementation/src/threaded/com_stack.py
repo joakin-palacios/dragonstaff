bc4d901d35b664c2bfb5d2f1a32c8961c23ca721
joakin-palacios <joakinpalacios@gmail.com>
2025-06-05 15:17:40 +0200

    trying chatgpts suggest for index out of bla bla bla

--- Dragonstaff/implementation/src/threaded/com_stack.py
+++ Dragonstaff/implementation/src/threaded/com_stack.py
@@ -2,7 +2,8 @@ import network
 import setupnfunctions as snf
 import socket
 import time
-
+import webpage
+import parser
 
 def connect(Lead):
     if Lead:
@@ -29,7 +30,8 @@ def connect(Lead):
     
 def threaded_connection():
     # Create a socket (IPv4, TCP)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # Bind to an address and port
     s.bind(('192.168.4.1', 80))
@@ -46,14 +48,44 @@ def threaded_connection():
             print(f"Connected by {addr}")
 
             # Receive data from the client
-            data = conn.recv(1024)
-            print(f"Received: {data}")
+            raw_request = conn.recv(2048)
+            print(raw_request)
+            
+            request_str = raw_request.decode()
+            request_line = request_str.split('\r\n')[0]
+            print("Request line:", request_line)
 
-            # Send a response
-            conn.sendall(b'Hello from server!')
-
-            # Close the connection
+            # Split and safely extract path
+            parts = request_line.split()
+            if len(parts) >= 2:
+                method = parts[0]
+                path = parts[1]
+                
+                if path == '/inquisition':
+                    print(f'{snf.status}  {snf.color}  {snf.co_color}  {snf.wait}')
+                    conn.sendall(bytes(f'{snf.status}  {snf.color}  {snf.co_color}  {snf.wait}', 'utf-8'))
+                else:
+                    parser.modes(path)
+                    html = webpage.webpage(snf.color, snf.status)
+                    conn.sendall(html)
+            else:
+                print("Malformed request:", request_line)
+                conn.sendall(b"HTTP/1.1 400 Bad Request\r\n\r\n")
+# 
+#             try:
+#                 raw_request=raw_request.split()[1]
+#             except OSError as e:
+#                 print('connection error ' + str(e.errno) + " " + str(e))
+#             if raw_request == b'inquisition':
+#                 print (f'{snf.status}  {snf.color}  {snf.co_color}  {snf.wait}')
+#                 conn.sendall(bytes(f'{snf.status}  {snf.color}  {snf.co_color}  {snf.wait}', 'utf-8'))
+#             else:
+#                 parser.modes(raw_request)
+#                 html = webpage.webpage(snf.color, snf.status)
+#                 conn.sendall(html)
             conn.close()
         except OSError as e:
             print('connection error ' + str(e.errno) + " " + str(e))
+        finally :
+            pass
             
--- Dragonstaff/implementation/src/threaded/main.py
+++ Dragonstaff/implementation/src/threaded/main.py
@@ -5,8 +5,6 @@ import com_stack
 
 Lead = True
   
-
-
 async def main():
     chunks=uasyncio.create_task(snf.pixelones())
     ledBlink=uasyncio.create_task(snf.led_blink(10,700))
